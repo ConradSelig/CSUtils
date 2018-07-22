928bb5e17fb259e54434bee5b773d240fad72258
ConradSelig <conrad.selig@mines.sdsmt.edu>
2018-07-22 01:51:25 -0600

    Adding test suite assertions for the match_data() function.

--- CSUtils-test.py
+++ CSUtils-test.py
@@ -1,12 +1,28 @@
 import CSUtils
 import traceback
 
+from collections import OrderedDict
+
 
 def test_match_data():
-    return
+
+    errors = 0
+
+    try:
+        assert(CSUtils.match_data("a,c,e,f", "a,b,c,d,e") == "a,c,e,b,d")
+        assert(CSUtils.match_data([1, 3, 5, 6], [1, 2, 3, 4, 5]) == [1, 3, 5, 2, 4])
+        assert(CSUtils.match_data('{"a": [1, 2], "c": [5, 6], "e": [9, 10]}',
+                                  '{"a": [1, 2], "b": [3, 4], "c": [5, 6], "d": [7, 8], "e": [9, 10]}') ==
+               OrderedDict([('a', [1, 2]), ('c', [5, 6]), ('e', [9, 10]), ('b', [3, 4]), ('d', [7, 8])]))
+    except AssertionError:
+        errors += 1
+
+    return errors
 
 
 def test_line_count():
+
+    errors = 0
 
     file = open("CSUtils-test.py", "r")
     data = file.readlines()
@@ -18,14 +34,24 @@ def test_line_count():
         assert(CSUtils.count_project_lines(includes="CSUtils-test.py") == lines)
         assert(CSUtils.count_project_lines() != lines)
     except AssertionError:
-        print(traceback.format_exc())
+        errors += 1
 
-    return 0
+    return errors
 
 
 def test_switch():
-    return
+    return 0
+
+
+def run_tests():
+    test_results = 0
+
+    test_results += test_match_data()
+    test_results += test_line_count()
+    test_results += test_switch()
+
+    return test_results
 
 
 if __name__ == "__main__":
-    print(test_line_count())
+    print(run_tests())
